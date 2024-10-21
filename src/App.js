b5b2cb77af32ea86fef1e13c1094b8e12a72fa53
ADITHYADAS080 <adithyadas95@gmail.com>
2024-10-21 15:47:51 +0530

    update for responsive 1

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
-
+import { selectUser } from "./features/userSlice";
+import { useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
 function App() {
+  const user = useSelector(selectUser);
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 768px)' });
   return (
     <div className="App">
-      {/* Sidebar */}
       
       <Sidebar />
-      {/* Chat */}
       <Chat />
     </div>
   );
